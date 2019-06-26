643ca6c56b60c0152121f7434d776bfe27b947c0
Adrian <adrian.narducci@gmail.com>
2019-06-26 13:38:40 -0300

    added service and main activitiy
    just that.

--- app/src/main/java/net/adriann/randomuserapi/service/RandomService.java
+++ app/src/main/java/net/adriann/randomuserapi/service/RandomService.java
@@ -1,7 +1,38 @@
 package net.adriann.randomuserapi.service;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 
 public class RandomService {
 
+    private static RandomService instance;
+    private static String BASE_URL = "https://randomuser.me/";
+    private Retrofit retrofit;
+
+    public static RandomService getInstance() {
+        if (instance == null) {
+            instance = new RandomService();
+        }
+        return instance;
+    }
+
+    private RandomService() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient.Builder client;
+        client = new OkHttpClient.Builder()
+                .addInterceptor(interceptor);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client.build())
+                .build();
+    }
+
+    public RandomUseApi getRandomUseApi(){return retrofit.create(RandomUseApi.class);}
 }
--- app/src/main/java/net/adriann/randomuserapi/service/RandomUseApi.java
+++ app/src/main/java/net/adriann/randomuserapi/service/RandomUseApi.java
@@ -1,14 +1,18 @@
 package net.adriann.randomuserapi.service;
 
+import net.adriann.randomuserapi.model.Response;
+
+import retrofit2.Call;
+
 import retrofit2.http.GET;
 
 public interface RandomUseApi {
 
 
     @GET("api/?results=1")
-    public Call<UserResponse> get1User();
+    public Call<Response> get1User();
 
     @GET("api/?results=50")
-    public Call<UserResponse> getUsers();
+    public Call<Response> getUsers();
 
 }
